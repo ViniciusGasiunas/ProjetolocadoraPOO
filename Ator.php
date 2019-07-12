f5a64fe820ed884deb5a4f760d5b590db21287bd
ViniciusGasiunas <50845838+ViniciusGasiunas@users.noreply.github.com>
2019-07-12 14:34:17 -0300

    Add files via upload

---
+++ Ator.php
@@ -0,0 +1,28 @@
+<?php
+require_once
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+public function criarAtor($nome, $sobrenome){
+    global $db;
+    if($nome != null && $sobrenome != null){
+    $query = $db->prepare("INSERT INTO ator(primeiro_nome")
+    }
+}
